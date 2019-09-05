93ae0d4d5d165387e6d5bbb4b7a76d8167ef99e7
Daniel Fryar <daniel.fryar@gmail.com>
2019-09-05 16:11:10 -0500

    Finished lesson on Controllers

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -1,19 +1,23 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 class HelloController {
+
+    private long count = 0;
 
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
         return "<h2>Hello from Spring!</h2>";
     }
-
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name) {
+        return "<h1>Hello " + name + "!</h1>";
+    }
     @PostMapping("/hello")
     @ResponseBody
     public String goodbye() {
@@ -24,5 +28,12 @@ class HelloController {
     @ResponseBody
     public String goodbyeGoodbye() {
         return "You waved goodbye";
+    }
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable long number) {
+        count += number;
+        return "Our count is now at "+ count +"!";
     }
 }
