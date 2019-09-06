bb8138d10ecf9ce32870afa6d9858a9164886775
Fernando Mendoza <fernandoza@gmail.com>
2019-09-06 12:16:56 -0500

    feat: finish the lecture about views
    - Added an extra example for CSS files linked

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HomeController {
 
@@ -13,10 +16,19 @@ public class HomeController {
     {
         String userSession = "ana";
         String role = "mortal";
+
+        List<String> colors = new ArrayList<>();
+
+        colors.add("blue");
+        colors.add("red");
+        colors.add("green");
+        colors.add("white");
+
         if(userSession.equals("fer")){
             role = "admin";
         }
         viewModel.addAttribute("role", role);
+        viewModel.addAttribute("colors" , colors);
         return "home";
     }
 }
