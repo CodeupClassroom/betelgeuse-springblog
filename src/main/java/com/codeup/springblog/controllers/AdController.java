93b7070eb66ce7b964554199a99f26de2d346c51
Fernando Mendoza <fernandoza@gmail.com>
2019-09-09 16:36:35 -0500

    feat: add Edit and Delete example

--- src/main/java/com/codeup/springblog/controllers/AdController.java
+++ src/main/java/com/codeup/springblog/controllers/AdController.java
@@ -39,6 +39,30 @@ public class AdController {
         return "ads/index";
     }
 
+    @GetMapping("/ads/{id}/edit")
+    public String edit(@PathVariable long id, Model viewModel) {
+        Ad ad = adDao.findOne(id);
+        viewModel.addAttribute("ad", ad);
+        return "ads/edit";
+    }
+
+    @PostMapping("/ads/{id}/edit")
+    public String update(@PathVariable long id,
+                         @RequestParam(name = "title") String title,
+                         @RequestParam(name = "description") String description,
+                         Model viewModel) {
+        Ad adToBeUpdated = adDao.findOne(id);
+        adToBeUpdated.setTitle(title);
+        adToBeUpdated.setDescription(description);
+        adDao.save(adToBeUpdated);
+        return "redirect:/ads/" + adToBeUpdated.getId();
+    }
+
+    @PostMapping("/ads/{id}/delete")
+    public String delete(@PathVariable long id){
+        adDao.delete(id);
+        return "redirect:/ads";
+    }
 //    @GetMapping("/posts/create")
 //    @ResponseBody
 //    public String createPostForm() {
