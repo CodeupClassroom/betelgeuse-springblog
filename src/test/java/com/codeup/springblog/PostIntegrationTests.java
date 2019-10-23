507d38f552bab7ec86ca085ea92579c087dc5996
Fernando Mendoza <fernandoza@gmail.com>
2019-10-23 16:05:30 -0500

    feat: setup the test file

---
+++ src/test/java/com/codeup/springblog/PostIntegrationTests.java
@@ -0,0 +1,15 @@
+package com.codeup.springblog;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringblogApplication.class)
+@AutoConfigureMockMvc
+public class PostIntegrationTests {
+
+
+
+}
